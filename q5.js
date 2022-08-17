f342ef9a406d7a6887ec1eb6ccbecaf0ddd489e3
Mohamed Alashkar <74485715+MohamedAlashkarkamel@users.noreply.github.com>
2022-08-17 16:52:53 +0200

    Delete q5.js

--- q5.js
+++
@@ -1,9 +0,0 @@
-var y = Number(prompt())
-var x = Number(prompt())
-
-console.log(`Sum = ${x+y}`)
-console.log(`Difference ${x}-${y} = ${x-y}`)
-console.log(`Difference ${y}-${x} = ${y-x}`)
-
-console.log(`Product = ${x*y}`)
-console.log(`Dividend = ${x/y}`)
